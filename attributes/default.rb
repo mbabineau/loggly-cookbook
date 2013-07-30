00184054f177e4a255995d68205efdf6c127762a
Mike Babineau <michael.babineau@gmail.com>
2013-07-30 10:11:04 -0700

    Use latest loggly python module with critical fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default[:loggly][:loggly_python][:version] = "0.1.4"
+default[:loggly][:loggly_python][:version] = "0.1.5"
 default[:loggly][:username] = ""
 default[:loggly][:password] = ""
 default[:loggly][:domain] = ""
